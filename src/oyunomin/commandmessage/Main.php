3596ab9562a149a2654226b814047881bdc5ee14
oyunomin0109 <68724268+oyunomin0109@users.noreply.github.com>
2021-02-08 23:12:10 +0900

    Update Main.php

--- src/oyunomin/commandmessage/Main.php
+++ src/oyunomin/commandmessage/Main.php
@@ -1,13 +1,28 @@
 <?php
-
-namespace oyunomin\helloworld;
-
-use pocketmine\{Player,Server};
-use pocketmine\event\Listener;
+namespace oyunomin\commandmessage; //このファイルのパスを書くよ
+//使うもののパスを書くよ
+use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\{Command,CommandSender}; //コマンド関連を扱う場合はこれを使うよ
 
-class Main extends PluginBase implements Listener{
-    public function onEnable(){
-        $this->getServer()->getLogger()->info("helloworld");
+class Main extends PluginBase implements Listener {
+    public function onEnable() {
+        $this -> getServer() -> getPluginManager() -> registerEvents($this,$this); //eventListener(イベント呼び出しに使う)にこのクラスを指定するよ。この場合はMainを指定してることになるよ
+    }
+    //$senderにはコマンドを打った人、$commandにはcommand,$labelにはコマンドの/?の?の部分、$argsには/? a bのaとbの部分が配列としてはいるよ。$args[0]にはa、$args[1]にはbが入るよ。
+    public function onCommand(CommandSender $sender,Command $command,string $label,array $args):bool {
+        if($label === 'message') { //$labelがhelloか判断するよ
+            if(!$sender instanceOf Player) { //$sender(コマンドを打った人)がプレイヤーかどうかを判断するよ。!付けた場合はプレイヤーではないってことになるよ
+                $sender -> sendMessage('ゲーム内で実行してください'); //コマンドを打った人に"ゲーム内で実行してください"と送るよ。
+                return true; //trueを返して処理終了だよ。これをしないとエラーを吐くよ
+            }
+            $sender ->getName(); //送信者の名前を取得するよ
+            $sender->getPlayer(); //プレイヤーを取得するよ
+            $sender -> sendMessage("こんにちは".$sender."さん");//送り主に"こんにちは、(player名)さん"と送るよ。
+            return true; //trueを返して処理終了だよ。これをしないとエラーを吐くよ
+        }
+        return false; //falseを返して処理終了だよ。これがないとエラーを吐くよ
     }
 }
